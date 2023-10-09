4dc66a17c5681f2f9a3c3703d9887289a9cd004e
“AndrewAmir003” <andrewamir003@gmail.com>
2023-10-09 15:48:14 +0100

    a lot of models, controllers and routers

--- server/routers/library.js
+++ server/routers/library.js
@@ -4,9 +4,13 @@ const bookController = require('../controllers/library.js');
 
 const bookRouter = Router();
 
-
-
-
+bookRouter.get("/",bookController.index);
+bookRouter.get("/:id",bookController.showID);
+bookRouter.get("/:title",bookController.showTitle);
+bookRouter.get("/:isbn", bookController.showISBN);
+bookRouter.post("/", bookController.create)
+bookRouter.patch("/:id", bookController.update)
+bookRouter.delete("/:id", bookController.destroy)
 
 
 
