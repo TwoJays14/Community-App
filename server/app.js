4370b9f0fc365d2538f8c6f1a650a8569bab969b
“AndrewAmir003” <andrewamir003@gmail.com>
2023-10-09 12:03:14 +0100

    app,js, index.js, setup.js, connect.js finished need database

--- server/app.js
+++ server/app.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+// const libraryRouter = require('./routers/library');
+const logger = require('morgan');
 
+const app = express();
+
+app.use(cors());
+app.use(logger('dev')) 
+app.use(express.json());
+
+// app.use('/library', libraryRouter);
+
+app.get('/', (req, res) => res.send('Hello there!'))
+app.post('/', (req, res) => res.status(405).send('Not allowed!'))
+
+module.exports = app
